33856c328b3b7de230653bc3c1aeac897744e61b
c@c.com <c@c.com>
2017-11-30 02:22:56 +0000

    .

---
+++ Demo.PropertySearch/Demo.PropertySearch.RestApi/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Demo.PropertySearch.RestApi.Helpers;
+using Demo.PropertySearch.RestApi.Models;
+
+namespace Demo.PropertySearch.RestApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private readonly NavigationLinkService _linkLinkService;
+
+        public HomeController()
+        {
+            _linkLinkService = new NavigationLinkServiceFactory().Create(this);
+        }
+
+        [Route(ApiRoute.Home.Route, Name = ApiRoute.Home.Name)]
+        public dynamic Get()
+        {
+            var result = new HomeModel
+            {
+                Version = this.GetType().Assembly.GetName().Version.ToString(),
+                Links = new[]
+                    {
+                        _linkLinkService.CreateLink<ApiRoute.Stock>("stock")
+                    }
+                    .Where(link => link != null)
+                    .ToArray()
+            };
+            return Ok(result);
+        }
+    }
+}
